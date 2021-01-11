c9cd8ae2871b5d2ef8541cfc66eca435686205e2
aliensmart <kaoua17@gmail.com>
2021-01-11 05:47:37 -0500

    fixed the padding of the order data

--- lib/widgets/orderItem.dart
+++ lib/widgets/orderItem.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'dart:math';
 
 import '../provider/orders.dart' as ord;
 
-class OrderItem extends StatelessWidget {
+class OrderItem extends StatefulWidget {
   final ord.OrderItem order;
 
   OrderItem(this.order);
+
+  @override
+  _OrderItemState createState() => _OrderItemState();
+}
+
+class _OrderItemState extends State<OrderItem> {
+  var _expended = false;
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -14,14 +22,46 @@ class OrderItem extends StatelessWidget {
       child: Column(
         children: [
           ListTile(
-            title: Text('\$${order.amount}'),
-            subtitle:
-                Text(DateFormat('dd/MM/yyyy hh:mm:ss').format(order.dateTime)),
+            title: Text('\$${widget.order.amount}'),
+            subtitle: Text(DateFormat('dd/MM/yyyy hh:mm:ss')
+                .format(widget.order.dateTime)),
             trailing: IconButton(
-              icon: Icon(Icons.expand_more),
-              onPressed: () {},
+              icon: Icon(_expended ? Icons.expand_less : Icons.expand_more),
+              onPressed: () {
+                setState(() {
+                  _expended = !_expended;
+                });
+              },
             ),
           ),
+          if (_expended)
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+              height: min(widget.order.products.length * 20.0 + 10, 180),
+              child: ListView(
+                children: widget.order.products
+                    .map(
+                      (prod) => Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            prod.title,
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            '${prod.quantity} x \$${prod.price}',
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                    .toList(),
+              ),
+            ),
         ],
       ),
     );
